f18c69258bdcfba1c2b19eb502665c3e04230efb
Savindu <savinduruhunuhewa@gmail.com>
2026-08-13 10:43:53 +0530

    Convert the BGR into RGB

--- main.py
+++ main.py
@@ -10,3 +10,23 @@ cropped=img[250:775,310:885]
 cv2.imshow('Cropped Image',cropped)
 cv2.waitKey(0)
 cv2.destroyAllWindows
+
+## convert the BGR into  RGB 
+img_RGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+# Plot the image using   matplotlib library
+plt.subplot(1,2,1)
+plt.imshow(img_RGB)
+
+plt.subplot(1,2,2)
+plt.imshow(img_RGB)
+plt.show()
+
+kali=cv2.imread('download.jpg',cv2.IMREAD_COLOR)
+kali_RGB=cv2.cvtColor(kali,cv2.COLOR_BGR2RGB)
+print(kali.shape)
+plt.subplot(2,2,3)
+plt.imshow(img_RGB)
+
+plt.subplot(2,2,2)
+plt.imshow(kali_RGB)
+plt.show()
