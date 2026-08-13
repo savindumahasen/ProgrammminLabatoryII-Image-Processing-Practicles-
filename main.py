8ab222e3759cffe5b595466f513df441eaee622e
Savindu <savinduruhunuhewa@gmail.com>
2026-08-13 11:01:32 +0530

    Resize the image

--- main.py
+++ main.py
@@ -9,10 +9,14 @@ cv2.imshow('Image',img)
 cropped=img[250:775,310:885]
 cv2.imshow('Cropped Image',cropped)
 cv2.waitKey(0)
-cv2.destroyAllWindows
+cv2.destroyAllWindows()
+
+## resize the image
+img_resize= cv2.resize(img,(800,900))
+
 
 ## convert the BGR into  RGB 
-img_RGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+img_RGB=cv2.cvtColor(img_resize,cv2.COLOR_BGR2RGB)
 # Plot the image using   matplotlib library
 plt.subplot(1,2,1)
 plt.imshow(img_RGB)
@@ -22,8 +26,11 @@ plt.imshow(img_RGB)
 plt.show()
 
 kali=cv2.imread('download.jpg',cv2.IMREAD_COLOR)
-kali_RGB=cv2.cvtColor(kali,cv2.COLOR_BGR2RGB)
-print(kali.shape)
+## image resize
+kali_resize=cv2.resize(kali,(1000,1200))
+kali_RGB=cv2.cvtColor(kali_resize,cv2.COLOR_BGR2RGB)
+
+print(kali_resize.shape)
 plt.subplot(2,2,3)
 plt.imshow(img_RGB)
 
